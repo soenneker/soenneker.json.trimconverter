2cfbc0088a4abad3611ff14b881f0b238240e16b
Jake Soenneker <jake@soenneker.com>
2023-03-30 20:13:18 -0500

    Initial commit

---
+++ src/TrimInputJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Soenneker.Json.TrimConverter;
+
+/// <summary>
+/// A System.Text.Json converter attribute for automatic string trimming
+/// </summary>
+public class TrimJsonConverter : JsonConverter<string?>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? result = reader.GetString()?.Trim();
+        return result;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value);
+    }
+}
